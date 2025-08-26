b3c6068d6bacb25e62c581a82e5c927c5ca04099
cutedogs2 <joao.falcao034@academico.ufgd.edu.br>
2025-08-26 16:45:51 -0400

    first commit

---
+++ back-end/src/servidor.ts
@@ -0,0 +1,16 @@
+import cors from "cors";
+import express from "express";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import RotasUsuário from "./rotas/rotas-usuário";
+import RotasProfessor from "./rotas/rotas-professor";
+const app = express();
+const PORT = process.env.PORT
+const CORS_ORIGIN = process.env.CORS_ORIGIN;
+app.use(cors({ origin: CORS_ORIGIN }));
+app.use(express.json());
+app.use("/usuarios", RotasUsuário);
+app.use("/professores", RotasProfessor);
+app.listen(PORT || 3333);
+const conexão = createConnection();
+export default conexão;
---
+++ front-end/src/utilitários/formatar-perfil.js
@@ -0,0 +1,8 @@
+
+export default function formatarPerfil(perfil) {
+  switch(perfil) {
+    case "professor": return "Professor";
+    case "aluno": return "Aluno";
+    default: return;
+  }
+};
